d02aacae730a3ac35834464c2037f4c41edba467
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-05-13 13:49:24 -0400

    base layout complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import "./App.css";
+
 const App = () => {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <header id="birdHeader">
+        <h1 id="testing">Bird Sanctuary</h1>
+      </header>
+      <section id="widgets">
+        <section id="cartSection">
+          <h2>Cart</h2>
+        </section>
+        <section id="birdSection">
+          <p>bird 1</p>
+          <p>bird 2</p>
+          <p>bird 3</p>
+          <p>bird 4</p>
+          <p>bird 5</p>
+          <p>bird 6</p>
+          <p>bird 7</p>
+          <p>bird 8</p>
+          <p>bird 9</p>
+        </section>
+        <section id="checkoutSection">
+          <h2>Checkout</h2>
+        </section>
+      </section>
     </div>
   );
 };
