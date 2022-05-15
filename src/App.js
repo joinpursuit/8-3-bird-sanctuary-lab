b2cd0d229aa3f7b7fc3f3f1040eb4020377767f0
Andres Varon <anvaron@gmail.com>
2022-05-15 12:08:48 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from "react";
+import birdData from "./data/birds.js"
+import "./App.css";
+
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <main>
+      <aside>
+        <section className="cart">
+          <h2>Cart</h2>
+        </section>
+        <section className="checkout">
+        <h2>Checkout</h2>
+        </section>
+      </aside>
+      <article>
+        {birdData.map(({ name, img, amount, id }) => (
+          <div className='card'>
+            <img src={img}/>
+            <h4 key={id}> {name} </h4>
+            <p> {amount} </p>
+          </div>
+        ))}
+      </article>
+    </main>
   );
 };
 
