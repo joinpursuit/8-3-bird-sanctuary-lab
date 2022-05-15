29f0eb3b8ba4b29027386bc9ac637087dea70c59
Lortiz528 <lortiztech@gmail.com>
2022-05-14 21:43:35 -0400

    created 3 components and linked to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
-};
+import React from 'react';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import Birdcards from './components/Birdcards';
+import birdData from './data/birds';
 
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <>
+        <Cart />
+        <Checkout />
+        <Birdcards />
+      </>
+    );
+  }
+}
 export default App;
---
+++ src/components/Birdcards.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import birdData from '../data/birds';
+
+class Birdcards extends React.Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default Birdcards;
