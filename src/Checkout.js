c8c914ed7c04aa3532ba45d5a97f6dad323d9b36
Edwin Codrington <edwincodrington@gmail.com>
2022-05-16 01:54:07 -0400

    Checkout form complete

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,49 +4,83 @@ export default class Checkout extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			valid: true,
+			valid: false,
+			firstName: "",
+			lastName: "",
+			email: "",
+			zip: 0,
 		};
 	}
 	handleSubmission = (event) => {
 		event.preventDefault();
 		const { value } = event.target;
 		console.log(value);
-		//check for valid input (no empty fields)
 		if (this.state.valid) {
 			alert("You have adopted birds. Thank You!");
 			//after this, clearcart()
 		} else {
-			alert("");
+			alert("no");
 		}
+	};
+	handleFirstName = (event) => {
+		this.setState({ firstName: event.target.value });
+	};
+	handleLastName = (event) => {
+		this.setState({ lastName: event.target.value });
+	};
+	handleEmail = (event) => {
+		this.setState({ email: event.target.value });
+	};
+	handleZip = (event) => {
+		this.setState({ zip: event.target.value });
+	};
+	finalFormCheck = () => {
+		//check for valid input (no empty fields)
+		if (this.state.firstName && this.state.lastName && this.state.email && this.state.zip) {
+			this.setState({ valid: true });
+		} else this.setState({ valid: false });
 	};
 	render() {
 		return (
 			<div>
 				<h2>Checkout</h2>
 				<p className="hidden">No items to check out.</p>
-				<form onSubmit={this.handleSubmission}>
+				<form
+					className="Checkout"
+					onChange={this.finalFormCheck}
+					onSubmit={this.handleSubmission}
+				>
 					<label>
 						{" "}
 						First Name
 						<br />
-						<input type="text" placeholder="Alex" />
+						<input
+							onChange={this.handleFirstName}
+							type="text"
+							placeholder="Alex"
+							required="yes"
+						/>
 					</label>
 					<label>
 						Last Name
 						<br />
-						<input type="text" placeholder="Williams" />
+						<input onChange={this.handleLastName} type="text" placeholder="Williams" />
 					</label>
 					<label>
 						Email
 						<br />
-						<input type="text" placeholder="name@email.com" />
+						<input
+							onChange={this.handleEmail}
+							type="text"
+							placeholder="name@email.com"
+						/>
 					</label>
 					<label>
 						Zip Code
 						<br />
-						<input type="number" placeholder="91210" />
+						<input onChange={this.handleZip} type="number" placeholder="91210" />
 					</label>
-					<button>Submit</button>
+					<input type="submit" text="Submit" />
 				</form>
 			</div>
 		);
