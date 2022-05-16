c288b2e7e235dd3c1555124d9609f20f98d53d42
named.josie <named.josie@gmail.com>
2022-05-16 08:24:34 -0400

    made changes to birdInfo.js file

---
+++ src/Components/BirdsInfo.js
@@ -0,0 +1,37 @@
+import React from "react";
+import birdData from "../data/birds";
+import './birds.css';
+class Birdcard extends React.Component{
+    render(){
+        let birdy= birdData.map((bird)=>{
+            return(
+              <div className="card">
+                  <h4>{bird.name}</h4>
+                    <img src={bird.img} alt="bird"/>
+                  <p>Price: ${bird.amount}</p>
+                  <button>Adopt</button>
+              </div>
+            )
+        })
+        return(
+            <div>{birdy}</div>
+        )
+    }
+}
+export default Birdcard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
