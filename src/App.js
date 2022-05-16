9e189fe8ebde569834824834c56e7401c07feb97
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-15 22:51:42 -0400

    form and app edited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-const App = () => {
-  return (
+import React from "react";
+import birdData from "./data/birds";
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  render(){ return (
     <div>
       <h1>Hello, world!</h1>
     </div>
   );
+  }
 };
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+class Form extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        userInput: "",
+        result: "",
+        func: "",
+        input: [],
+      };
+    }
+    selec = (event) => {
+      this.setState({
+        func: event.target.value,
+      });
+    };
+    case = (event) => {
+      event.preventDefault();
+      switch (this.state.func) {
+        case "sum":
+          this.setState({
+            result: this.state.input.reduce((a, b) => a + b),
+          });
+  
+          for (const num in numbers) {
+            if (Number(numbers[num]) > max) {
+              max = Number(numbers[num]);
+              mode = [num];
+            } else if (Number(numbers[num]) === max) {
+              mode.push(num);
+            }
+          }
+          this.setState({
+            result: mode.join(),
+          });
+          break;
+          default:
+          this.setState({
+            result: "Invalid input.",
+          });
+      }
+    };
+    refreshPG = (event) => {
+        let value = event.target.value;
+        this.setState({
+        userInput: value,
+        input: value.split(",").map((item) => Number(item)),
+      });
+    };
+  
+    render() {
+      return ( <>
+        <form>
+          <input id="values" name="values" type="text" onChange={this.refreshPG} />
+          <select
+            id="operation"
+            name="operation"
+            onChange={this.selec}
+          >
+            <option value=""></option>
+            <option value="sum">sum</option>
+            <option value="average">average</option>
+            <option value="mode">mode</option>
+          </select>
+          <button type="submit" onClick={this.case}>
+            Calculate
+          </button>
+        </form>
+        <div>
+          <p>{this.state.input.join()}</p>
+          <p>{this.state.result}</p>
+        </div>
+      </>
+    );
+  }
+  }
+  export default Form;
