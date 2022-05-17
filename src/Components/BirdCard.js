f3ec9dc8f3518af90140733f7a8e24fe68500c33
bowersjuan <bowersjuan@gmail.com>
2022-05-17 03:50:42 -0400

    Update class for BirdCard.js component

--- src/Components/BirdCard.js
+++ src/Components/BirdCard.js
@@ -3,7 +3,7 @@ import React from "react";
 class BirdCard extends React.Component {
   render() {
     return (
-      <div className="bird-card">
+      <div className="card">
         <h5>{this.props.name}</h5>
         <p>Price: {this.props.amount}</p>
         <img src={this.props.img} alt="Bird Profile Picture" width="100px" />
