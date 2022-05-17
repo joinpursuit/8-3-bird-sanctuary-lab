d0cbe7c80f3161345cf7fb4f5a04061ba42ad1c0
lily08 <liliangelxd@gmail.com>
2022-05-17 07:02:21 -0400

    It can get total of birds to cart work

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -8,9 +8,6 @@ function Cart({ cart }) {
   })
 
   let sum = 0
-  // for (let car of cart) {
-  //   sum += car.amount
-  // }
 
   let amount = cart.map((bird) => {
     return bird.amount
@@ -20,11 +17,12 @@ function Cart({ cart }) {
     return (sum = a + b)
   }, 0)
 
+
   return (
     <div className='Cart'>
       <h2>Cart</h2>
       <h4>Discount:</h4>
-      <h4>Total:{sum}</h4>
+      <h4>Total:${sum}</h4>
       <ol>{cartItems}</ol>
     </div>
   )
