02d90f235faff8ff45b35ec550d7714486e0a5fc
bowersjuan <bowersjuan@gmail.com>
2022-05-17 07:27:32 -0400

    Update Cart.js component

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,25 +1,61 @@
 import react from "react";
+import bonusItems from "../data/bonusItems";
 
 class Cart extends react.Component {
   render() {
-    let birdsArray = this.props.adoptedBirdsArray;
+    let adoptedBirdsArray = this.props.adoptedBirdsArray;
     let birdCost = this.props.birdCost;
 
     return (
       <div className="Cart">
         <h5>Cart</h5>
-        <p>Discount:</p>
+        <p>Discount: {adoptedBirdsArray.length < 3 ? "0%" : "10%"}</p>
         <h4>Total: ${birdCost}</h4>
         <ol>
-          {birdsArray.map((bird) => (
+          {adoptedBirdsArray.map((bird) => (
             <li>{bird}</li>
           ))}
         </ol>
         <p>Your donations have qualified you for the following items:</p>
-        <ul>
-          <li>Bonus Item</li>
-        </ul>
+        {bonusItemsAdder(birdCost)}
       </div>
+    );
+  }
+}
+
+function bonusItemsAdder(total) {
+  if (total >= 100 && total <= 300) {
+    return (
+      <ul>
+        <li>{bonusItems[0]}</li>
+      </ul>
+    );
+  }
+  if (total > 300 && total <= 500) {
+    return (
+      <ul>
+        <li>{bonusItems[0]}</li>
+        <li>{bonusItems[1]}</li>
+      </ul>
+    );
+  }
+  if (total > 500 && total <= 1000) {
+    return (
+      <ul>
+        <li>{bonusItems[0]}</li>
+        <li>{bonusItems[1]}</li>
+        <li>{bonusItems[2]}</li>
+      </ul>
+    );
+  }
+  if (total > 1000) {
+    return (
+      <ul>
+        <li>{bonusItems[0]}</li>
+        <li>{bonusItems[1]}</li>
+        <li>{bonusItems[2]}</li>
+        <li>{bonusItems[3]}</li>
+      </ul>
     );
   }
 }
