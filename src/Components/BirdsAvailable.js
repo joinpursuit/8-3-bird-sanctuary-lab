3845a8f3df896b32337cb8f5197060110d64bff9
arupay <arupay11374@gmail.com>
2022-05-17 13:17:37 -0400

    removed comments

--- src/Components/BirdsAvailable.js
+++ src/Components/BirdsAvailable.js
@@ -4,24 +4,6 @@ import "./BirdsAvailable.css";
 class BirdsAvailable extends React.Component {
   render() {
     const { birds, addToCart } = this.props;
-    // return (
-    //   <section id="birdmarket">
-    //     {birds.map((bird) => {
-    //       const { name, amount, img, id } = bird;
-    //       return (
-    //         <article className="card birds" key={id}>
-    //           <h3>{name}</h3>
-    //           <p>Price: ${amount}</p>
-    //           <img src={img} alt={name} width="300" height="300" />
-
-    //           <button onClick={addToCart} type="button" value={id}>
-    //             Adopt
-    //           </button>
-    //         </article>
-    //       );
-    //     })}
-    //   </section>
-    // );
     return (
       <section id="birdmarket">
         {birds.map((bird) => {
