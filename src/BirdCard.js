a78f10395f28a3c0f57ea9dcfbc9095d54186fd1
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-16 23:42:08 -0400

    BirdCard

--- src/BirdCard.js
+++ src/BirdCard.js
@@ -1,14 +1,20 @@
-import "./BirdCard.css"
+import React from "react";
 
 
-function BirdCard ( {bird: {name,amount, img},cart}) {
-    cart()
-    return(
-        <article className="birds card">
-            <h4>{name}</h4>
-            <p>Price: ${amount}</p>
-            <img src={img} alt={name} />
-            <button OnClick={cart}>Adopts</button>
-        </article>
-    )
+class BirdCard extends React.Component  {
+    render() {
+        const {img, amount, name, id} = this.props.birbInfo;
+        const {adoption} = this.props;
+
+        return (
+            <div className="card birds">
+                <h4>{name}</h4>
+                <p>Price: ${amount}</p>
+                <img src={img} alt={name} />
+                <button onClick={()=> adoption(id,name,amount)}>Adopt</button>
+            </div>
+        )
+    }
 }
+
+export default BirdCard;
