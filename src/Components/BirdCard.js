16ec206067c1f36d5a864f3381629dc95b9c692b
bowersjuan <bowersjuan@gmail.com>
2022-05-17 04:18:20 -0400

    Update BirdCard.js component

--- src/Components/BirdCard.js
+++ src/Components/BirdCard.js
@@ -4,10 +4,13 @@ class BirdCard extends React.Component {
   render() {
     return (
       <div className="card">
-        <h5>{this.props.name}</h5>
+        <br></br>
+        <p>
+          <strong>{this.props.name}</strong>
+        </p>
         <p>Price: {this.props.amount}</p>
         <img src={this.props.img} alt="Bird Profile Picture" width="100px" />
-        <button>Adopt</button>
+        <button className="adopt-button">Adopt</button>
       </div>
     );
   }
