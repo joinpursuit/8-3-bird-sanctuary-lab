08bf17e1b4111657b566d5121cb580f5a0aba844
Jonathan Scheiber <jon@scheiber.me>
2022-05-17 01:10:14 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import birdData from "./data/birds";
+import bonusItems from "./data/bonusItems";
+import BirdCards from "./components/BirdCards";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import { useState } from "react";
+import "./App.css";
+
 const App = () => {
+  const [cart, setCart] = useState([]);
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="App">
+      <BirdCards birdData={birdData} cart={cart} setCart={setCart} />
+      <Cart
+        birdData={birdData}
+        cart={cart}
+        setCart={setCart}
+        bonusItems={bonusItems}
+      />
+      <Checkout cart={cart} setCart={setCart} />
     </div>
   );
 };
