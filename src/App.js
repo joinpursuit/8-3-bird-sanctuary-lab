3f70f353b9c21bae3436320c1759a050fe1de914
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-16 23:54:56 -0400

    app edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import birdData from "./data/birds";
+import bonusItems from "./data/bonusItems";
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import BirdSection from "./BirdSection";
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
- birdInCart: [],
+ birbsInCart: [],
     }
     this.birdData = this.props
   }
@@ -14,20 +18,39 @@ class App extends React.Component {
   //   return <CartItem name={bird.name} amount={bird.amount} />
   // }
 
+  adoption = (id, name, amount) => {
+    let birbCopy = [...this.state.birbsInCart];
+    birbCopy.push({id,name,amount});
+    this.setState({birbsInCart: birbCopy,})
+  }
+
   birbCart = (bird) => {
     console.log("...Adding bird:", bird.name);
     const newCart = [...this.state.cart, bird];
     this.setState({cart: newCart})
   };
 
-  render(){ return (
+  checkingOut = (evt) => {
+    evt.preventDefault();
+    if (this.state.birbsInCart.length > 0) {
+      alert("You have adopted birds. Thank You!");
+      this.setState({birbsInCart});
+    } else {
+      alert("You haven't adopted any birds yet!");
+    }
+  }
+
+  render(){
+    const {birds, bonusItems,} = this.props 
+    return (
     <div className="parent">
       <div className="left">
-        <Cart birds={this.birdData} />
+        <BirdSection birdData={birdData} adoption={this.adoption}/>
+        <Cart bonusItems={bonusItems} birbsInCart={this.state.birbsInCart} />
         <Checkout />
         </div>
         <BirdProfile birdData ={this.props} />
-      <h1>Hello, world!</h1>
+      <Checkout checkingOut={this.checkingOut} />
     </div>
   );
   }
