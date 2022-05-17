8419b9309e1d9132b816252d59f7a4f52d035357
bowersjuan <bowersjuan@gmail.com>
2022-05-17 09:14:20 -0400

    Update App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
-};
+import react from "react";
+import Birds from "./Components/Birds";
+import Cart from "./Components/Cart";
+import Checkout from "./Components/Checkout";
+import "./App.css";
+
+class App extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      adoptedBirdsArray: [],
+      birdCost: 0,
+    };
+  }
+
+  handleClick(event) {
+    const birdName = event.target.parentNode.children[1].innerText;
+    const birdCost = parseInt(
+      event.target.parentNode.children[2].innerText.slice(8)
+    );
+
+    this.setState({
+      adoptedBirdsArray: this.state.adoptedBirdsArray.concat(
+        `${birdName}: $${birdCost}`
+      ),
+      birdCost: this.state.birdCost + birdCost,
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Cart
+          adoptedBirdsArray={this.state.adoptedBirdsArray}
+          birdCost={this.state.birdCost}
+        />
+        <Birds onClick={(event) => this.handleClick(event)} />
+        <Checkout />
+      </div>
+    );
+  }
+}
 
 export default App;
